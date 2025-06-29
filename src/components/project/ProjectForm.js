c42bd8e6f9d8a40d676f6b3d172f6214fe3d5970
NatanOliveiraslv <natanoliveiraslv14@gmail.com>
2025-06-29 00:48:44 -0300

    first commit

---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,29 @@
+import Input from '../form/Input'
+
+import styles from './ProjectForm.module.css'
+
+const ProjectForm = () => {
+    return (
+        <form className={styles.form}>
+            <Input 
+                type="text" 
+                text="Nome do projeto" 
+                name="name" 
+                placeholder="Insira o nome do projeto"
+            />
+            <div>
+                <input type="text" placeholder="insira o orçamento total" />
+            </div>
+            <div>
+                <select name="category_id">
+                    <option disabled selected>Selecione a categoria</option>
+                </select>
+            </div>
+            <div>
+                <input type="submit" value="Criar Projeto" />
+            </div>
+        </form>
+    )
+}
+
+export default ProjectForm;
